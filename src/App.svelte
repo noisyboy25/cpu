1fd20057352c130273fbd5627a82e834837bdfdc
noisyboy25 <dj402and@gmail.com>
2022-09-08 22:52:13 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Program from './lib/Program.svelte';
+</script>
+
+<main>
+  <div class="card">
+    <Program />
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/lib/Program.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let program = 'Hello world';
+  $: lines = program.split('\n');
+</script>
+
+<textarea bind:value={program} />
+{#each lines as line}
+  <p>{line}</p>
+{/each}
