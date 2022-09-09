a11aa8d001e525786c579bf5721de450dcce8095
noisyboy25 <dj402and@gmail.com>
2022-09-09 16:13:17 +0300

    Add program input

--- src/lib/Program.svelte
+++ src/lib/Program.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
+  type Command = { op: string; args: string[] };
   let program = 'Hello world';
-  $: lines = program.split('\n');
+  $: commands = program.split('\n').map(parseCommand);
+
+  function parseCommand(line: string): Command {
+    const words = line.split(' ');
+    const operator = words[0];
+    const args = words.slice(1);
+    return { op: operator, args };
+  }
 </script>
 
 <textarea bind:value={program} />
-{#each lines as line}
-  <p>{line}</p>
-{/each}
+<table>
+  {#each commands as cmd}
+    <tr>
+      <td class="op">
+        {cmd.op}
+      </td>
+      {#each cmd.args as arg}
+        <td class="arg">
+          {arg}
+        </td>
+      {/each}
+    </tr>
+  {/each}
+</table>
+
+<style>
+  .op {
+    color: orange;
+  }
+  .arg {
+    color: lightyellow;
+  }
+  .arg::before {
+    content: '[';
+  }
+  .arg::after {
+    content: ']';
+  }
+</style>
