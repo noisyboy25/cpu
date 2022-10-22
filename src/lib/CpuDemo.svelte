91c494744c7e5e641e85f1f8d8dd30b891b27a0c
noisyboy25 <dj402and@gmail.com>
2022-10-22 05:17:11 +0300

    Start cleaning the UI

--- src/lib/CpuDemo.svelte
+++ src/lib/CpuDemo.svelte
@@ -4,6 +4,7 @@
 
   import { Status } from './cpu';
   import CpuStatus from './CpuStatus.svelte';
+  import LoadedProgram from './LoadedProgram.svelte';
 
   import { cpu } from './stores';
 
@@ -11,7 +12,7 @@
     'MOV 1\nMOVXR 1\nMOV 5\nMOVXR 0\nJRXZ 12\nSUB 1\nMOVXR 0\nMOVRX 1\nADD 2\nMOVXR 1\nMOVRX 0\nJMP 3\n';
   let status: Status;
   let pc: number;
-  let loadedProgram: number[];
+  let program: number[];
 
   let rx: number;
   let r: number[];
@@ -19,12 +20,10 @@
   let output = '';
   let error = '';
 
-  let hint = false;
-
   cpu.subscribe((value) => {
     status = value.status;
     pc = value.pc;
-    loadedProgram = value.cMem;
+    program = value.cMem;
     rx = value.rx;
     r = value.r;
   });
@@ -82,70 +81,12 @@
   <button on:click={() => step()}>Step</button>
   <button on:click={() => reset()}>Reset</button>
   <button on:click={() => clear()}>Clear</button>
-  <button
-    class:active={hint}
-    on:click={() => {
-      hint = !hint;
-    }}>Hint</button
-  >
 </div>
 <div class="main">
-  <div>
-    <div class="loaded-commands">
-      <div class="row head">
-        <div>#</div>
-        <div>OP</div>
-      </div>
-      {#each loadedProgram as cmd, i}
-        <div class="row" class:active={status != Status.DONE && i === pc - 1}>
-          <div>{i}</div>
-          <div class="op">
-            {cmd.toString(2).padStart(32, '0')}
-          </div>
-        </div>
-      {/each}
-    </div>
-  </div>
+  <LoadedProgram {program} {pc} />
   <CpuStatus {status} {pc} {rx} {r} {error} />
-  {#if !hint}
-    <CodeBlock text={output} />
-  {:else}
-    <div class="op-list" />
-  {/if}
+  <CodeBlock text={output} />
 </div>
 
 <style>
-  .active {
-    background-color: #333;
-  }
-  .active > div {
-    background-color: #333;
-  }
-  .loaded-commands {
-    display: grid;
-    grid-template-columns: 5ch 1fr;
-    max-height: 20em;
-    text-align: left;
-    padding: 0 1em;
-  }
-  .loaded-commands .row {
-    display: contents;
-  }
-  .loaded-commands .head {
-    font-weight: bold;
-  }
-  .loaded-commands div {
-    display: inline-block;
-    text-align: center;
-  }
-  .op {
-    color: orange;
-  }
-  .op-list {
-    margin: 0;
-    padding: 1em;
-    text-align: left;
-    background-color: #333;
-    border-radius: 8px;
-  }
 </style>
---
+++ src/lib/LoadedProgram.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let program: number[];
+  export let pc: number;
+</script>
+
+<div class="loaded-commands">
+  <div class="row head">
+    <div>#</div>
+    <div>OP</div>
+  </div>
+  {#each program as cmd, i}
+    <div class="row" class:active={i === pc - 1}>
+      <div>{i}</div>
+      <div class="op">
+        {cmd.toString(2).padStart(32, '0')}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .loaded-commands {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  .row {
+    display: contents;
+  }
+</style>
